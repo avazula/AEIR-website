6e2dc03aa5da0c9f0dc25038ff8cf87a5c887cc9
avazula <mbrunet@insa-rennes.fr>
2018-08-14 09:29:39 +0200

    Cleanup

--- blb/Proto/threads/pthreads.cmake
+++
@@ -1,9 +0,0 @@
-find_package( Threads )
-if( CMAKE_USE_PTHREADS_INIT )
-    find_library( PTHREAD_LIBRARY pthread )
-    message( STATUS "Using pthread in " ${PTHREAD_LIBRARY} )
-
-else()
-    message( ERROR "No pthread found" )
-
-endif()
